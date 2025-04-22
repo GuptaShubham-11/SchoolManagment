c100830afe4b868d23e83d6bf26e1362a22e2eb2
GuptaShubham-11 <shu919847@gmail.com>
2025-04-22 17:19:42 +0530

    add DB_PORT

--- src/db/db.js
+++ src/db/db.js
@@ -7,6 +7,7 @@ const connectToDb = async () => {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
+            port: process.env.DB_PORT,
         });
         console.log('Connected to the database successfully!');
         return connection;
